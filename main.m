fb27c4b8db11c64221ccd94a52bfb629baa78395
bodecharlotte <charlotte.bode@googlemail.com>
2019-07-04 08:14:26 +0200

    some_message

--- main.m
+++ main.m
@@ -2,82 +2,38 @@ format long
 % 
 %  h = 10:10:100;
 %  for j = 1:length(h)
-    %Initialize Parameters
-    %Initialize binarys
-    har = 2;
 
-    % method == 0 BEFD, == 1 TSSM , == 2 BEPS
-    method = 0;
+    %Initialize boundary h == 0  box, h = 1 harmonic, h = 2 harmonic,
+    %double well, box
+    har = 0;
+
 
     %initialize Constants
     Co = constants;
-    m = Co.m;
+    
     properties = struct();
-    P = Parameters(0.001 ,10000, [-8,8], [-4,4], 64,  0, Co);
-    if method == 0 
-        [P.X, P.Y]= meshgrid(P.x, P.y); 
-    else 
-        [P.X, P.Y]= meshgrid(P.x, P.y); 
-    end
+    P = Parameters(0.0005 ,100000, [-8,8], [-4,4], [-4,4], 128,  0, Co);
+    
+    [P.X, P.Y, P.Z]= meshgrid(P.x, P.y, P.z); 
+    
     %Initialize Potentials
     Po = Potential(P,Co,har);
-    figure(123)
-    mesh(P.X, P.Y, Po.pot)
+%     figure(123)
+%     mesh(P.Y(:,:), P.Z(:,:), Po.pot(:,:))
 
     %initialize matrix or spectral operators
 
 
 
-    if method == 0
 
-        psi = Energy((Co.omega_y*Co.omega_x)^(1/4)/(pi)^(1/2) *exp(-(Co.omega_x*P.X.^2+Co.omega_y*P.Y.^2)/2), P);
-        wave = psi.wave;
+    psi = Spectral((Co.omega_y*Co.omega_x*Co.omega_z)^(1/4)/(pi)^(3/4) *exp(-(Co.omega_x*P.X.^2+(Co.omega_y)*P.Y.^2+ Co.omega_z * P.Z.^2)/2), P, Po, Co);
 
-        % renormalizing for imaginary time 
-        M = P.hinitial_x * P.hinitial_y *sum(psi.density(:));
-        psi.wave = sqrt(complex(1/M)) * psi.wave;
-
-        [psi, properties, Po]=gpu_init(psi, properties, method, Po);
-        psi = mat(psi, P, Po);
-        A = (eye(length(psi.matrix)) + P.timestep*psi.matrix);
-        A = sparse(A);
-        e = ones(P.res,1);
-        T = spdiags([-1/2*e 0*e 1/2*e], -1:1, P.res, P.res);
-        der = kron(speye(P.res),T/P.hinitial_x) + kron(T/P.hinitial_y,speye(P.res));
-
-    elseif method ==1   
-        psi = Spectral((Co.omega_y*Co.omega_x)^(1/4)/(pi)^(1/2) *exp(-(Co.omega_x*P.X.^2+(Co.omega_y)*P.Y.^2)/2), P, Po, Co, m);
-
-        %Renormalize
-        M = P.hinitial_x * P.hinitial_y .* sum(psi.density(:));
-        psi.wave = sqrt(complex(1/M)) * psi.wave;
+    %Renormalize
+    M = P.hinitial_x * P.hinitial_y * P.hinitial_z .* sum(psi.density(:));
+    psi.wave = sqrt(complex(1/M)) * psi.wave;
 
 
-        der = 0;
-    elseif  method == 2
-        psi = Spectral((Co.omega_y/Co.omega_x)^(1/4)/(pi)^(1/2) *exp(-(P.X.^2+(Co.omega_y/Co.omega_x)*P.Y.^2)/2), P, Po, Co, m);
-
-        %Renormalize
-        M = P.hinitial_x * P.hinitial_y .* sum(psi.density(:));
-        psi.wave = sqrt(complex(1/M)) * psi.wave;
-
-    end 
-
-
-
-
-
-
-
-
-
-    % figure(123)
-    % spy(A)
-    % 
-    % fprintf('press enter\n')
-    % pause
-
-    C = Conservation(psi, P, Po, method, der);
+    C = Conservation(psi, P, Po);
 
     %Store properties 
         properties.wave = psi.wave;
@@ -86,9 +42,8 @@ format long
         properties.mu(1) = C.mu;
         properties.op_pulse = psi.density;
 
-    if method == 1
-        [psi, properties, Po]=gpu_init(psi, properties, method, Po);
-    end
+    [psi, properties, Po]=gpu_init(psi, properties, Po);
+  
 
     ln = 2;
     psi.wold = psi.wave;
@@ -103,60 +58,19 @@ format long
 
 
         %Compute
-        if method ==0 
-            properties.ham = gpuArray(properties.ham);
-            properties.mass = gpuArray(properties.mass);
-            properties.mu = gpuArray(properties.mu);
-            tic
-
-            B = A + P.timestep*diag(reshape(P.nonlinear*abs(psi.wold).^2, [], 1));
-            B = sparse(B);
-            psi.wave = bicgstab(B,psi.wave(:));
-    %         psi.wave = B\psi.wave(:);
-            elapsed_time_solve = toc;
+       
+        tic
+        psi = Compute(psi); 
+        elapsed_time_solve = toc;
+        psi.density = real(conj(psi.wave).* psi.wave);
 
 
+        %Renormalize
+        M = P.hinitial_x * P.hinitial_y .* sum(psi.density(:));
+        psi.wave = sqrt(complex(1/M)) * psi.wave;
 
 
-            psi.wave = reshape(psi.wave, P.res, P.res);
-            psi.density = real((psi.wave).*conj(psi.wave));
-
-
-
-
-
-            % renormalizing for imaginary time
-            % M = trapz(P.y,trapz(P.x, psi.density,2));
-            % psi.wave = sqrt(complex(1/M)) * psi.wave;
-
-            temp = (P.hinitial_x * P.hinitial_y) * sum(abs(psi.wave(:)).^2);
-            psi.wave = sqrt(1/temp) * psi.wave;
-
-        elseif method == 1 
-            tic
-            psi = Compute(psi); 
-            elapsed_time_solve = toc;
-            psi.density = real(conj(psi.wave).* psi.wave);
-
-
-            %Renormalize
-            M = P.hinitial_x * P.hinitial_y .* sum(psi.density(:));
-            psi.wave = sqrt(complex(1/M)) * psi.wave;
-
-        elseif method ==2
-
-            psi = Compute_EI(P,psi,Po);
-
-            psi.density = real((psi.wave).*conj(psi.wave));
-
-            % renormalizing for imaginary time
-            M = P.hinitial_x * P.hinitial_y * sum(psi.density(:));
-            psi.wave = sqrt(1/M) * psi.wave;
-
-        end
-
-
-        C = Conservation(psi, P, Po, method, der);
+        C = Conservation(psi, P, Po);
 
 
 
@@ -171,17 +85,17 @@ format long
         %compute l_inf norm
         if count==0
             properties.diff = abs(psi.wold-psi.wave);
-            reldiff = max(max(properties.diff));
+            reldiff = max(max(max(properties.diff)));
             rele = abs(Enold-C.En);
             properties.relEn(ln) =rele;
-            properties.rel_diff(ln) = max(max(properties.diff));
+            properties.rel_diff(ln) = max(max(max(properties.diff)));
         else 
             if mod(i, 2^count)==0 %compute l_inf norm of wave with same difference than before
                 properties.diff = abs(psi.wold-psi.wave);
-                reldiff = max(max(properties.diff));
+                reldiff = max(max(max(properties.diff)));
                 rele = abs( Enold-C.En);
                 properties.relEn(ln) = rele;
-                properties.rel_diff(ln) = max(max(properties.diff));
+                properties.rel_diff(ln) = max(max(max(properties.diff)));
             end 
         end
 %         if mod(i, 10) == 0
@@ -195,7 +109,7 @@ format long
             fprintf('Ground state after %i timesteps\n', ln);
             figure(1)
             subplot(2,1,1)
-            mesh(P.X, P.Y, psi.density)
+            mesh(P.X(:,:), P.Y(:,:), psi.density(:,:))
             hold off
             title(['time t= ', num2str(t)])
             drawnow
@@ -208,41 +122,33 @@ format long
             break
         end
 
-       if method == 0 
-            properties.residual(ln-1) = max(max(abs(properties.mu(ln-1)*psi.wold - reshape(psi.matrix *psi.wold(:),P.res, P.res))));
-        elseif method == 1
-            properties.residual(ln-1) = max(max(abs(properties.mu(ln-1)*psi.wold - (-0.5*real(ifftn((psi.k.^2 + psi.w.^2).* fftn(psi.wold))) + Po.pot .* psi.wold+ P.nonlinear*abs(psi.wold).^2.* psi.wold))));
-        elseif method == 2
-            properties.residual(ln-1) = max(abs(properties.mu(ln-1)*psi.wold - psi.matrix * psi.wold));
-       end
+      
 
-%         if mod(i, 2000) ==0
-%             if method ==1
-%                 P.timestep_imag = P.timestep_imag/2;
-%                 psi.R = exp(-1i*(Po.pot + P.nonlinear*abs(psi.wave).^2)*P.timestep_imag/2);
-%                 psi.K = exp(-1i * P.timestep_imag *(psi.k_squared+ psi.w_squared)/2); 
-%             else
-%                 P.timestep = P.timestep/2;
-%             end
-%             count = count +1;
-%         end 
+        if mod(i, 10000) ==0
+
+            P.timestep_imag = P.timestep_imag/2;
+            psi.R = exp(-1i*(Po.pot + P.nonlinear*abs(psi.wave).^2)*P.timestep_imag/2);
+            psi.K = exp(-1i * P.timestep_imag *(psi.k_squared+ psi.w_squared+ psi.v_squared)/2); 
+
+            count = count +1;
+        end 
 
         %         plot energy and density
          %Plot while computing
-            if mod(i,100)==0
-                figure(1)
-                subplot(2,1,1)
-                mesh(P.X, P.Y, psi.density)
-                hold off
-                title(['time t= ', num2str(t)])
-                drawnow
-                subplot(2,1,2)
-                plot(properties.ham(1:ln))
-                hold off 
-                title('Energy Evolution imaginary timestepping')
-                drawnow
-
-            end
+%             if mod(i,100)==0
+%                 figure(1)
+%                 subplot(2,1,1)
+%                 mesh(P.X(:,:), P.Y(:,:), psi.density(:,:))
+%                 hold off
+%                 title(['time t= ', num2str(t)])
+%                 drawnow
+%                 subplot(2,1,2)
+%                 plot(properties.ham(1:ln))
+%                 hold off 
+%                 title('Energy Evolution imaginary timestepping')
+%                 drawnow
+% 
+%             end
 
 
         psi.wold = psi.wave;
@@ -258,6 +164,7 @@ format long
     fprintf('Time until Ground state in [ms] %f\n',t*1.37);
     fprintf('xrms, yrms: [%f %f]\n', [C.xrms, C.yrms]);
     fprintf('Relative energy and density error [%e %e]\n', [rele, reldiff]);
+    dlmwrite('exact2', psi.density, 'delimiter',',','-append', 'precision', 16)
 %     if method == 1
 %         dlmwrite('grounddata_Box_tssm', psi.density, 'delimiter',',','-append')
 %         s = 'timestep, maxiterations, count, rel energy, reldiff, energy, chem, xrms, yrms, har:';
